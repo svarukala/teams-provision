244cd6b49a85cfb13027970867ea569ada6c4d69
Srinivas Varukala <svarukala@gmail.com>
2022-05-27 18:36:14 -0400

    first commit

---
+++ GraphHelper.cs
@@ -0,0 +1,264 @@
+using Azure.Core;
+using Azure.Identity;
+using Microsoft.Graph;
+
+class GraphHelper
+{
+    // Settings object
+    private static Settings? _settings;
+    // User auth token credential
+    private static DeviceCodeCredential? _deviceCodeCredential;
+    // Client configured with user authentication
+    private static GraphServiceClient? _userClient;
+
+    // App-ony auth token credential
+    private static ClientSecretCredential? _clientSecretCredential;
+    // Client configured with app-only authentication
+    private static GraphServiceClient? _appClient;
+
+    private static void EnsureGraphForAppOnlyAuth()
+    {
+        // Ensure settings isn't null
+        _ = _settings ??
+            throw new System.NullReferenceException("Settings cannot be null");
+
+        if (_clientSecretCredential == null)
+        {
+            _clientSecretCredential = new ClientSecretCredential(
+                _settings.TenantId, _settings.ClientId, _settings.ClientSecret);
+        }
+
+        if (_appClient == null)
+        {
+            _appClient = new GraphServiceClient(_clientSecretCredential,
+                // Use the default scope, which will request the scopes
+                // configured on the app registration
+                new[] { "https://graph.microsoft.com/.default" });
+        }
+    }
+    private static void EnsureGraphForAppOnlyAuth(string[] scopes)
+    {
+        // Ensure settings isn't null
+        _ = _settings ??
+            throw new System.NullReferenceException("Settings cannot be null");
+
+        if (_clientSecretCredential == null)
+        {
+            _clientSecretCredential = new ClientSecretCredential(
+                _settings.TenantId, _settings.ClientId, _settings.ClientSecret);
+        }
+
+        if (_appClient == null)
+        {
+            _appClient = new GraphServiceClient(_clientSecretCredential,
+                scopes);
+        }
+    }
+    public static void InitializeGraphForAppOnlyAuth(Settings settings)
+    {
+        _settings = settings;
+        EnsureGraphForAppOnlyAuth();
+    }
+
+    public static void InitializeGraphForUserAuth(Settings settings,
+        Func<DeviceCodeInfo, CancellationToken, Task> deviceCodePrompt)
+    {
+        _settings = settings;
+
+        _deviceCodeCredential = new DeviceCodeCredential(deviceCodePrompt,
+            settings.AuthTenant, settings.ClientId);
+
+        _userClient = new GraphServiceClient(_deviceCodeCredential, settings.GraphUserScopes);
+    }
+
+    public static async Task<string> GetUserTokenAsync()
+    {
+        // Ensure credential isn't null
+        _ = _deviceCodeCredential ??
+            throw new System.NullReferenceException("Graph has not been initialized for user auth");
+
+        // Ensure scopes isn't null
+        _ = _settings?.GraphUserScopes ?? throw new System.ArgumentNullException("Argument 'scopes' cannot be null");
+
+        // Request token with given scopes
+        var context = new TokenRequestContext(_settings.GraphUserScopes);
+        var response = await _deviceCodeCredential.GetTokenAsync(context);
+        return response.Token;
+    }
+
+    public static async Task<string> GetAppOnlyTokenAsync()
+    {
+        // Ensure credential isn't null
+        _ = _clientSecretCredential ??
+            throw new System.NullReferenceException("Graph has not been initialized for app-only auth");
+
+        // Request token with given scopes
+        var context = new TokenRequestContext(new[] { "https://graph.microsoft.com/.default" });
+        var response = await _clientSecretCredential.GetTokenAsync(context);
+        return response.Token;
+    }
+
+    public static Task<User> GetUserAsync()
+    {
+        // Ensure client isn't null
+        _ = _userClient ??
+            throw new System.NullReferenceException("Graph has not been initialized for user auth");
+
+        return _userClient.Me
+            .Request()
+            .Select(u => new
+            {
+                // Only request specific properties
+                u.DisplayName,
+                u.Mail,
+                u.UserPrincipalName
+            })
+            .GetAsync();
+    }
+
+    public static Task<IGraphServiceUsersCollectionPage> GetUsersAsync()
+    {
+        EnsureGraphForAppOnlyAuth();
+        // Ensure client isn't null
+        _ = _appClient ??
+            throw new System.NullReferenceException("Graph has not been initialized for app-only auth");
+
+        return _appClient.Users
+            .Request()
+            .Select(u => new
+            {
+                // Only request specific properties
+                u.DisplayName,
+                u.Id,
+                u.Mail
+            })
+            // Get at most 25 results
+            .Top(25)
+            // Sort by display name
+            .OrderBy("DisplayName")
+            .GetAsync();
+    }
+
+    public async static Task<string> CreateNewTeamAsync()
+    {
+        EnsureGraphForAppOnlyAuth();
+        //EnsureGraphForAppOnlyAuth(new[] { "https://graph.microsoft.com/Team.Create" });
+
+        // Ensure client isn't null
+        _ = _appClient ??
+            throw new System.NullReferenceException("Graph has not been initialized for app-only auth");
+
+        var team = new Team
+        {
+            DisplayName = "AutoGen-" + System.IO.Path.GetRandomFileName(),
+            Description = "My Sample Team’s Description",
+            Members = new TeamMembersCollectionPage()
+            {
+                new AadUserConversationMember
+                {
+                    Roles = new List<String>()
+                    {
+                        "owner"
+                    },
+                    AdditionalData = new Dictionary<string, object>()
+                    {
+                        {"user@odata.bind", "https://graph.microsoft.com/v1.0/users('4cb08dcb-b50e-4ee6-9712-03fd4c746a6c')"}
+                    }
+                }
+            },
+            AdditionalData = new Dictionary<string, object>()
+            {
+                {"template@odata.bind", "https://graph.microsoft.com/v1.0/teamsTemplates('standard')"}
+            }
+        };
+        var result = await _appClient.Teams.Request().AddAsync(team);
+        //Console.WriteLine("Created team: " + result.ToString());
+        return team.DisplayName;
+    }
+
+    public async static Task<IGraphServiceGroupsCollectionPage> ListGroupsWithoutTeamsAsync()
+    {
+        EnsureGraphForAppOnlyAuth();
+        //EnsureGraphForAppOnlyAuth(new[] { "https://graph.microsoft.com/Group.Read.All" });
+
+        // Ensure client isn't null
+        _ = _appClient ??
+            throw new System.NullReferenceException("Graph has not been initialized for app-only auth");
+
+        var queryOptions = new List<QueryOption>()
+        {
+            new QueryOption("$count", "true")
+        };
+        var groups = await _appClient.Groups
+        .Request(queryOptions)
+        .Header("ConsistencyLevel", "eventual")
+        .Filter("groupTypes/any(c:c eq 'Unified') and NOT(resourceProvisioningOptions/any(x:x eq 'Team'))")
+        .Select("id, displayName") //, resourceProvisioningOptions")
+        .GetAsync();
+
+        return groups;
+    }
+
+    public async static Task<string> TeamifyGroupAsync(string groupId)
+    {
+        EnsureGraphForAppOnlyAuth();
+        //EnsureGraphForAppOnlyAuth(new[] { "https://graph.microsoft.com/Group.Read.All" });
+
+        // Ensure client isn't null
+        _ = _appClient ??
+            throw new System.NullReferenceException("Graph has not been initialized for app-only auth");
+
+        var team = new Team
+        {
+            MemberSettings = new TeamMemberSettings
+            {
+                AllowCreatePrivateChannels = true,
+                AllowCreateUpdateChannels = true
+            },
+            MessagingSettings = new TeamMessagingSettings
+            {
+                AllowUserEditMessages = true,
+                AllowUserDeleteMessages = true
+            },
+            FunSettings = new TeamFunSettings
+            {
+                AllowGiphy = true,
+                GiphyContentRating = GiphyRatingType.Strict
+            }
+        };
+
+        var result = await _appClient.Groups[groupId].Team
+            .Request()
+            .PutAsync(team);
+        return "Teamified group successfully: "+ result.WebUrl;
+        /*
+        //Alternate way to teamify a group
+        var team = new Team
+        {
+            AdditionalData = new Dictionary<string, object>()
+            {
+                {"template@odata.bind", "https://graph.microsoft.com/v1.0/teamsTemplates('standard')"},
+                {"group@odata.bind", $"https://graph.microsoft.com/v1.0/groups('{groupId}')"}
+            }
+        };
+
+        await _appClient.Teams
+            .Request()
+            .AddAsync(team);
+        */
+    }
+
+    public static Task<IGraphServiceSitesCollectionPage> ListNonGroupSitesAsync()
+    {
+        EnsureGraphForAppOnlyAuth();
+        // Ensure client isn't null
+        _ = _appClient ??
+            throw new System.NullReferenceException("Graph has not been initialized for app-only auth");
+
+        return _appClient.Sites
+                .Request()
+                //.Filter("siteCollection/root ne null")
+                .Select("siteCollection,webUrl")
+                .GetAsync();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,286 @@
+﻿Console.WriteLine("Teams Provision (Using .NET 6)\n");
+
+var settings = Settings.LoadSettings();
+
+// Initialize Graph
+//InitializeGraph(settings);
+InitializeGraphAppOnly(settings);
+
+// Greet the user by name
+//await GreetUserAsync();
+
+int choice = -1;
+
+while (choice != 0)
+{
+    Console.WriteLine("Please choose one of the following options:");
+    Console.WriteLine("0. Exit");
+    Console.WriteLine("1. Display app-only access token");
+    Console.WriteLine("2. Display user access token");
+    Console.WriteLine("3. List users (app-only)");
+    Console.WriteLine("4. Create New Team (app-only)");
+    Console.WriteLine("5. List groups w/o Team (app-only)");
+    Console.WriteLine("6. Teamify group (app-only)");
+    Console.WriteLine("7. List sites w/o group (app-only)");
+    Console.WriteLine("8. Teamify site (app-only)");
+    try
+    {
+        choice = int.Parse(Console.ReadLine() ?? string.Empty);
+    }
+    catch (System.FormatException)
+    {
+        // Set to invalid value
+        choice = -1;
+    }
+    switch (choice)
+    {
+        case 0:
+            // Exit the program
+            Console.WriteLine("Goodbye...");
+            break;
+        case 1:
+            // Display access token
+            await DisplayAppOnlyAccessTokenAsync();
+            break;
+        case 2:
+            // List emails from user's inbox
+            await DisplayAccessTokenAsync();
+            break;
+        case 3:
+            // List users
+            await ListUsersAsync();
+            break;
+        case 4:
+            // Run any Graph code
+            await CreateNewTeamAsync();
+            break;
+        case 5:
+            // Run any Graph code
+            await ListGroupsWithoutTeamsAsync();
+            break;            
+        case 6:
+            // Run any Graph code
+            await TeamifyGroupAsync();
+            break;            
+        case 7:
+            // Run any Graph code
+            await ListNonGroupSitesAsync();
+            break;              
+        default:
+            Console.WriteLine("Invalid choice! Please try again.");
+            break;
+    }
+    /*
+    switch(choice)
+    {
+        case 0:
+            // Exit the program
+            Console.WriteLine("Goodbye...");
+            break;
+        case 1:
+            // Display access token
+            await DisplayAccessTokenAsync();
+            break;
+        case 2:
+            // List emails from user's inbox
+            await ListInboxAsync();
+            break;
+        case 3:
+            // Send an email message
+            await SendMailAsync();
+            break;
+        case 4:
+            // List users
+            await ListUsersAsync();
+            break;
+        case 5:
+            // Run any Graph code
+            await MakeGraphCallAsync();
+            break;
+        default:
+            Console.WriteLine("Invalid choice! Please try again.");
+            break;
+    }
+    */
+}
+
+void InitializeGraphAppOnly(Settings settings)
+{
+    GraphHelper.InitializeGraphForAppOnlyAuth(settings);
+}
+
+void InitializeGraph(Settings settings)
+{
+    GraphHelper.InitializeGraphForUserAuth(settings,
+        (info, cancel) =>
+        {
+            // Display the device code message to
+            // the user. This tells them
+            // where to go to sign in and provides the
+            // code to use.
+            Console.WriteLine(info.Message);
+            return Task.FromResult(0);
+        });
+}
+
+async Task GreetUserAsync()
+{
+    try
+    {
+        var user = await GraphHelper.GetUserAsync();
+        Console.WriteLine($"Hello, {user?.DisplayName}!");
+        // For Work/school accounts, email is in Mail property
+        // Personal accounts, email is in UserPrincipalName
+        Console.WriteLine($"Email: {user?.Mail ?? user?.UserPrincipalName ?? ""}");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error getting user: {ex.Message}");
+    }
+}
+
+async Task DisplayAccessTokenAsync()
+{
+    try
+    {
+        InitializeGraph(settings);
+        var userToken = await GraphHelper.GetUserTokenAsync();
+        Console.WriteLine($"User token: {userToken}");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error getting user access token: {ex.Message}");
+    }
+}
+
+async Task DisplayAppOnlyAccessTokenAsync()
+{
+    try
+    {
+        var appOnlyToken = await GraphHelper.GetAppOnlyTokenAsync();
+        Console.WriteLine($"AppOnly token: {appOnlyToken}");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error getting app-only access token: {ex.Message}");
+    }
+}
+
+async Task ListInboxAsync()
+{
+    // TODO
+}
+
+async Task SendMailAsync()
+{
+    // TODO
+}
+
+async Task ListUsersAsync()
+{
+    try
+    {
+        var userPage = await GraphHelper.GetUsersAsync();
+
+        // Output each users's details
+        foreach (var user in userPage.CurrentPage)
+        {
+            Console.WriteLine($"User: {user.DisplayName ?? "NO NAME"}");
+            Console.WriteLine($"  ID: {user.Id}");
+            Console.WriteLine($"  Email: {user.Mail ?? "NO EMAIL"}");
+        }
+
+        // If NextPageRequest is not null, there are more users
+        // available on the server
+        // Access the next page like:
+        // userPage.NextPageRequest.GetAsync();
+        var moreAvailable = userPage.NextPageRequest != null;
+
+        Console.WriteLine($"\nMore users available? {moreAvailable}");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error getting users: {ex.Message}");
+    }
+}
+
+async Task CreateNewTeamAsync()
+{
+    try
+    {
+        var teamName = await GraphHelper.CreateNewTeamAsync();
+        Console.WriteLine($"Created new team: {teamName}");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error creating team: {ex.Message}");
+    }
+}
+
+async Task ListGroupsWithoutTeamsAsync()
+{
+    try
+    {
+        var groups = await GraphHelper.ListGroupsWithoutTeamsAsync();
+        Console.WriteLine($"Total groups without teams: "+ groups.Count);
+        
+            
+        foreach (var group in groups.CurrentPage)
+        {
+            Console.WriteLine($"Name: {group.DisplayName ?? "NO NAME"}, ID: {group.Id}");
+        }
+
+        // If NextPageRequest is not null, there are more users
+        // available on the server
+        // Access the next page like:
+        // userPage.NextPageRequest.GetAsync();
+        var moreAvailable = groups.NextPageRequest != null;
+
+        Console.WriteLine($"\nMore groups available? {moreAvailable}");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error getting groups: {ex.Message}");
+    }
+}
+
+async Task TeamifyGroupAsync()
+{
+    try
+    {
+        Console.WriteLine("Enter group ID to teamify:");
+        var groupId = Console.ReadLine();
+        var result = await GraphHelper.TeamifyGroupAsync(groupId);
+        Console.WriteLine(result);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error getting groups: {ex.Message}");
+    }
+}
+
+async Task ListNonGroupSitesAsync()
+{
+    try
+    {
+        var sites = await GraphHelper.ListNonGroupSitesAsync();
+
+        // Output each users's details
+        foreach (var site in sites.CurrentPage)
+        {
+            Console.WriteLine($"SiteUrl: {site.WebUrl ?? "NO NAME"}");
+        }
+
+        // If NextPageRequest is not null, there are more users
+        // available on the server
+        // Access the next page like:
+        // userPage.NextPageRequest.GetAsync();
+        var moreAvailable = sites.NextPageRequest != null;
+
+        Console.WriteLine($"\nMore sites available? {moreAvailable}");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error getting users: {ex.Message}");
+    }
+}
